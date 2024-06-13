002fcbb355b67466428c89e74c118a4ecd15ce63
ttarreuu <tarahanifanfaza@gmail.com>
2024-06-13 13:25:50 +0700

    implement interval every 10 second and push data to api

--- App.tsx
+++ App.tsx
@@ -2,22 +2,16 @@ import { StyleSheet, Text, View, PermissionsAndroid, Button, Modal } from 'react
 import React, { useEffect, useState } from 'react';
 import Geolocation from 'react-native-geolocation-service';
 import ReactNativeForegroundService from "@supersami/rn-foreground-service";
+import NetInfo from '@react-native-community/netinfo';
 
 const App = () => {
   const [location, setLocation] = useState(null);
   const [modalVisible, setModalVisible] = useState(false);
+  const [list, setList] = useState([]);
 
   useEffect(() => {
-    requestLocationPermission()
-    
-
-    const interval = setInterval(() => {
-      startForegroundService();
-    }, 10000);
-
-    return () => {
-      clearInterval(interval);
-    };
+    requestLocationPermission();
+    startForegroundService();
   }, []);
 
   const requestLocationPermission = async () => {
@@ -67,25 +61,80 @@ const App = () => {
     });
 
     ReactNativeForegroundService.add_task(() => getCurrentLocation(), {
-      delay: 1000,
+      delay: 10000,
       onLoop: true,
       taskId: "taskid",
       onError: (e) => console.log(`Error logging:`, e),
     });
   };
 
+  const checkInternetConnection = async () => {
+    return await NetInfo.fetch().then((state) => state.isConnected);
+  };
+
+  const sendDataToApi = async (data) => {
+    try {
+      await fetch('https://6639cbd81ae792804beccbdc.mockapi.io/location/v1/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getData = () => {
+    fetch('https://6662b64562966e20ef09a745.mockapi.io/location/v2/users', {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((res) => {
+        setList(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleData = async ({dateTime, latitude, longitude}) => {
+    const isConnected = await checkInternetConnection();
+
+    if(isConnected) {
+      // await syncDataWithApi();
+      await sendDataToApi({dateTime, latitude, longitude});
+      getData();
+    }else {
+      //await sendDataToLocalDB({dateTime, latitude, longitude});
+    }
+  };
+
   const getCurrentLocation = () => {
     const currentDate = new Date();
     const dateTime = currentDate.toLocaleString();
     console.log(dateTime);
+
+
     Geolocation.getCurrentPosition(
       position => {
         if (position.mocked === false) {
+          const latitude = position.coords.latitude;
+          const longitude = position.coords.longitude;
+          const currDateTime = dateTime;
+
           console.log(position);
           setLocation(position);
+          handleData({dateTime, latitude, longitude});
         } else {
+          const latitude = 0;
+          const longitude = 0;
+          const currDateTime = dateTime;
+
           console.log("Fake GPS Detected");
           setModalVisible(true);
+          handleData({dateTime, latitude, longitude});
         }
       },
       error => {
